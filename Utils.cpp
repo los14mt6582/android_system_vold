04da13c73b4bfc56495bb19d1ba43e484dbf99c0
LineageOS-MTK <luzejij@binka.me>
2017-09-06 00:55:55 +0300

    Fix SD card name for old mtk
    Add flags SD_OLD_MTK

--- Utils.cpp
+++ Utils.cpp
@@ -198,6 +198,12 @@ status_t BindMount(const std::string& source, const std::string& target) {
 
 static status_t readMetadata(const std::string& path, std::string& fsType,
         std::string& fsUuid, std::string& fsLabel, bool untrusted) {
+#ifdef SD_OLD_MTK
+    fsType.clear();
+    fsUuid.clear();
+    fsLabel.clear();
+#endif
+
 #ifdef MINIVOLD
     char *val = NULL;
     val = blkid_get_tag_value(NULL, "TYPE", path.c_str());
